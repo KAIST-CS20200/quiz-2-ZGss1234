9975dcd26ec9230d291e81ef94bfa32b7c33ae36
PulleyBun <z22x22c22@naver.com>
2025-03-11 10:09:05 +0900

    ok

--- Math.fs
+++ Math.fs
@@ -2,8 +2,16 @@
 
 open System
 
+let f n =
+  if n%2UL = 0UL then n/2UL
+  else n*3UL+1UL
+
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  let rec iter num n = 
+    let result = f n
+    if result = 1UL then num
+    else iter (num+1) result
+  iter 1 (uint64 n)
